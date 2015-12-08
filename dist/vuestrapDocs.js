5fd33ed22361b94d7bc220388aeadb27d24f3e9d
Kris Zima <kris@mosquito.ie>
2015-12-08 14:51:55 +1000

    bump version to 0.2.3

--- dist/vuestrapDocs.js
+++ dist/vuestrapDocs.js
@@ -1283,7 +1283,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	module.exports = {
 		"name": "vuestrap-docs",
-		"version": "0.2.2",
+		"version": "0.2.3",
 		"description": "Vuestrap Docs Components are useed in docs pages to showcase web components.",
 		"library": "vuestrapDocs",
 		"repository": {
